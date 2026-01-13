57b48603d70822e00a868cbd3c39602904597f75
Nourou Diagouraga <diagourganourou@gmail.com>
2026-01-13 13:54:50 +0100

    Fix router: home as default route

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,7 @@
        <li><router-link to="/about">About</router-link></li>
       <li><router-link to="/projects">Projects</router-link></li>
       <li><router-link to="/skills">Skills</router-link></li>
-      <li><router-link to="/contact">Contact</router-link></li>
-     
+      
     </ul>
   </nav>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
 import Projects from '../views/Projects.vue'
 import Skills from '../views/Skills.vue'
-import Contact from '../views/Contact.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  },
+  {
+    path: '/skills',
+    name: 'Skills',
+    component: Skills
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/about', name: 'About', component: About },
-    { path: '/projects', name: 'Projects', component: Projects },
-    { path: '/skills', name: 'Skills', component: Skills },
-    { path: '/contact', name: 'Contact', component: Contact }
-    
-  ]
+  history: createWebHistory('/portfolio/'),
+  routes
 })
 
 export default router
